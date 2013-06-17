9b629de888cf12b05915b2f93c4866da8af9b817
Tobias Schultz <schultz.tobias@gmail.com>
2013-06-17 15:03:22 +0200

    github test line inserted

--- Where is my Ride?/WIMRViewController.h
+++ Where is my Ride?/WIMRViewController.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Tobias Schultz and Steffen Heberle. All rights reserved.
 //
 
+// github test line
+
 #import <UIKit/UIKit.h>
 
 @interface WIMRViewController : UIViewController
