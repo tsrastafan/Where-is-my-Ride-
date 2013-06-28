0c97a36b75dcf3a892fd7a0ef8d04d9805f49cc0
Tobias Schultz <schultz.tobias@gmail.com>
2013-06-28 16:07:56 +0200

    annotation added

---
+++ Where is my Ride?/Model/WIMRAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  WIMRAnnotation.m
+//  Where is my Ride?
+//
+//  Created by Tobias Schultz on 6/28/13.
+//  Copyright (c) 2013 Tobias Schultz and Steffen Heberle. All rights reserved.
+//
+
+#import "WIMRAnnotation.h"
+
+@implementation WIMRAnnotation
+
+@end
