199ca7d5fb18436d3d5ae12015abad227848135b
Tobias Schultz <schultz.tobias@gmail.com>
2013-08-25 08:33:44 +0200

    deletet junk files

--- Where is my Ride?/WIMRAppDelegate.m
+++ Where is my Ride?/WIMRAppDelegate.m
@@ -155,7 +155,12 @@
 
 #pragma mark - Application's Documents directory
 
-// Returns the URL to the application's Documents directory.
+
+
+/*! Returns the Application's Documents Directory
+ *
+ * returns Application Document Directory
+ */
 - (NSURL *)applicationDocumentsDirectory
 {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
