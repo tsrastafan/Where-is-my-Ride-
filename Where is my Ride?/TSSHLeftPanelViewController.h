54bd8cb7ed1db4a2ee5252758bbe70930c072ce6
Tobias Schultz <schultz.tobias@gmail.com>
2013-09-25 17:06:45 +0200

    added TSSHLeftPanelViewController

---
+++ Where is my Ride?/TSSHLeftPanelViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSSHLeftPanelViewController.h
+//  Where is my Ride?
+//
+//  Created by Tobias Schultz on 9/25/13.
+//  Copyright (c) 2013 Tobias Schultz and Steffen Heberle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSSHLeftPanelViewController : UIViewController
+
+@end
---
+++ Where is my Ride?/TSSHLeftPanelViewController.m
@@ -0,0 +1,38 @@
+//
+//  TSSHLeftPanelViewController.m
+//  Where is my Ride?
+//
+//  Created by Tobias Schultz on 9/25/13.
+//  Copyright (c) 2013 Tobias Schultz and Steffen Heberle. All rights reserved.
+//
+
+#import "TSSHLeftPanelViewController.h"
+
+@interface TSSHLeftPanelViewController ()
+
+@end
+
+@implementation TSSHLeftPanelViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
